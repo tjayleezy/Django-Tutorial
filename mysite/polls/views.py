2464a65b65dc7f0a3bbf7c35ce553b2a18456722
Leary <1522406496@mil>
2019-02-20 11:29:43 -0500

    Finished tutorial 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from .models import Question
+from django.utils import timezone
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
@@ -12,7 +13,7 @@ from django.urls import reverse
 from .models import Choice, Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html') 
     context = {'latest_question_list' : latest_question_list,}
     return render(request, 'polls/index.html', context)
@@ -26,6 +27,7 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
     
 
+    
 # ...
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
